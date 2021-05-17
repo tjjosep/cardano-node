7b8427777c8cf40544743ae706e84ac9d982b8f7
Tony Jose Joseph <tonyjjoseph@Tony-MacBook-Pro.hsd1.il.comcast.net>
2021-05-16 19:10:59 -0500

    [Running scripts]

--- terraform/cloudinit_config.tf
+++ terraform/cloudinit_config.tf
@@ -3,7 +3,7 @@ data "cloudinit_config" "this_script" {
   base64_encode = true
   part {
     content_type = "text/cloud-config"
-    content      = (templatefile("./scripts/user_data.tpl", {
+    content = (templatefile("./scripts/user_data.tpl", {
       loggroup      = aws_cloudwatch_log_group.this_log_group.id
       componentname = "${var.prefix}-cardano-node"
       region        = var.region
